e6e609816ac661c74516752e36b56d0ed06053d6
goldledoigt <gary@revolunet.com>
2012-10-20 20:16:22 +0200

    boostrap style + video into modal + file size

---
+++ public/javascripts/index.js
@@ -0,0 +1,31 @@
+var player;
+
+_V_("player").ready(function() {
+    player = this;
+});
+
+$(function() {
+
+    var modal = $('#modal'),
+        modalLabel = $('#modalLabel');
+
+    modal.on('hidden', function () {
+        player.pause();
+        player.bigPlayButton.show();
+    });
+
+    $('.link').click(function() {
+        var link = $(this);
+
+        modalLabel.html(link.attr('data-name'));
+
+        player.src([
+            {type: 'video/mp4', src: link.attr('data-path')},
+            {type: 'video/webm', src: link.attr('data-pathWebm')}
+        ]);
+
+        modal.modal('show');
+
+    });
+
+});
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,34 @@
+function formatNumber(number, decimals, dec_point, thousands_sep) {
+    var n = number, c = isNaN(decimals = Math.abs(decimals)) ? 2 : decimals;
+    var d = dec_point === undefined ? "," : dec_point;
+    var t = thousands_sep === undefined ? "." : thousands_sep, s = n < 0 ? "-" : "";
+    var i = parseInt(n = Math.abs(+n || 0).toFixed(c)) + "", j = (j = i.length) > 3 ? j % 3 : 0;
+ 
+    return s + (j ? i.substr(0, j) + t : "") + i.substr(j).replace(/(\d{3})(?=\d)/g, "$1" + t) + (c ? d + Math.abs(n - i).toFixed(c).slice(2) : "");
+}
+
+var formatFileSize = function(filesize) {
+    if (filesize >= 1073741824) {
+         filesize = formatNumber(filesize / 1073741824, 2, '.', '') + ' Gb';
+    } else {
+        if (filesize >= 1048576) {
+            filesize = formatNumber(filesize / 1048576, 2, '.', '') + ' Mb';
+        } else {
+            if (filesize >= 1024) {
+                filesize = formatNumber(filesize / 1024, 0) + ' Kb';
+            } else {
+                filesize = formatNumber(filesize, 0) + ' bytes';
+            }
+        }
+    }
+    return filesize;
+};
+
 exports.index = function(req, res) {
 
-    var data = [],
+    var info, stats, data = [],
         fs = require('fs'),
+        util = require('util'),
         cfg = require('../config'),
         root = cfg.path,
         path = req.params.path || '';
@@ -17,11 +44,13 @@ exports.index = function(req, res) {
         files.sort();
 
         files.forEach(function(file) {
+            stats = fs.statSync(root + '/' + file);
             data.push({
                 name: file,
                 path: path + '/' + file,
                 pathWebm: (path + '/' + file).split('MP4')[0] + 'webm',
-                isDirectory: fs.statSync(root + '/' + file).isDirectory()
+                size: formatFileSize(stats.size),
+                isDirectory: stats.isDirectory()
             });
         });
 
